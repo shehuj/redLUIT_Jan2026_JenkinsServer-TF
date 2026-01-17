d98c4b22a974430150ba05c461fd8bfeec5d7fae
Jenom <119628596+shehuj@users.noreply.github.com>
2026-01-17 07:38:43 -0600

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -97,7 +97,7 @@ variable "jenkins_ui_cidrs" {
     - Open to internet (NOT RECOMMENDED): ["0.0.0.0/0"]
   EOT
   type        = list(string)
-#  default     = ["0.0.0.0/0"]
+  default     = ["0.0.0.0/0"]
 
   validation {
     condition = alltrue([
