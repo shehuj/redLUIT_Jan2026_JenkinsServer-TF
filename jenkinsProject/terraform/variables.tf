181c6b6663eae27416c864501e235f4fe7c8b683
Jenom <119628596+shehuj@users.noreply.github.com>
2026-01-19 10:35:51 -0600

    Update variables.tf

--- jenkinsProject/terraform/variables.tf
+++ jenkinsProject/terraform/variables.tf
@@ -7,15 +7,17 @@ variable "aws_region" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
+  default     = "m5.xlarge"
 }
 
 variable "jenkins_sg_id" {
   description = "Security Group ID for Jenkins"
   type        = string
+  default = "sg-0bfcde60ef49a5dcd"
 }
 
 variable "key_pair" {
   description = "EC2 SSH Key Pair"
   type        = string
+  default     = "key"
 }
