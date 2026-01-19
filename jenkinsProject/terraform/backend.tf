34e66d7cf2f713f2d6134201cd39b0ffc9523ab8
Jenom <119628596+shehuj@users.noreply.github.com>
2026-01-19 11:04:50 -0600

    updated fkows

---
+++ jenkinsProject/terraform/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    # Note: Backend configuration doesn't support interpolation
+    # The actual key is set via -backend-config during terraform init in CI/CD
+    # For local use: terraform init -backend-config="key=terraform-states/dev/infra.tfstate"
+
+    bucket         = "ec2-shutdown-lambda-bucket"
+    key            = "jenkins-states/default/infra.tfstate"  # Overridden by -backend-config
+    region         = "us-east-1"
+    dynamodb_table = "dyning_table"
+    encrypt        = true
+  }
+}
--- jenkinsProject/terraform/outputs.tf
+++ jenkinsProject/terraform/outputs.tf
@@ -1,9 +1,14 @@
-output "public_ip" {
-  description = "Public IP of EC2"
+output "jenkins_public_ip" {
+  description = "The public IP address of the Jenkins server"
   value       = aws_instance.jenkins.public_ip
 }
 
-output "private_ip" {
-  description = "Private IP of EC2"
+output "jenkins_private_ip" {
+  description = "The private IP address of the Jenkins server"
   value       = aws_instance.jenkins.private_ip
+}
+
+output "jenkins_instance_id" {
+  description = "The ID of the Jenkins EC2 instance"
+  value       = aws_instance.jenkins.id
 }
